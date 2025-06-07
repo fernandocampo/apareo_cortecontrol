aab2778a8850aad2aa60cc26869b2c255b4848fa
fernandocampo <fernandocampow@gmail.com>
2025-06-07 20:12:56 -0300

    enunciado y main

---
+++ ej3.cpp
@@ -0,0 +1,35 @@
+/*
+
+Ejercicio 3: Corte de Control Múltiple
+Enunciado: Una escuela tiene registros de notas ordenados por curso y luego por alumno. Crear un programa que calcule el promedio de notas de cada alumno en cada curso.
+Estructura de datos:
+struct Nota {
+    string curso;
+    int legajo;
+    int nota;
+};
+Datos de prueba (ya ordenados por curso, luego por legajo):
+Curso "1A", Legajo 123: nota 8
+Curso "1A", Legajo 123: nota 7
+Curso "1A", Legajo 456: nota 9
+Curso "1B", Legajo 234: nota 6
+Curso "1B", Legajo 789: nota 8
+Curso "1B", Legajo 789: nota 10
+Resultado esperado:
+Curso 1A:
+  Legajo 123: Promedio 7.5
+  Legajo 456: Promedio 9.0
+Curso 1B:  
+  Legajo 234: Promedio 6.0
+  Legajo 789: Promedio 9.0
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    return 0;
+}
