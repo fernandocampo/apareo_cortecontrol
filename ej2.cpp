12dd9685b02a1ee9abed402249ac11a9792ae26a
fernandocampo <fernandocampow@gmail.com>
2025-06-07 20:05:46 -0300

    carga hardcodeada sin vector dinamico

--- ej2.cpp
+++ ej2.cpp
@@ -21,6 +21,63 @@ Vendedor 101: 8 libros totales
 Vendedor 102: 13 libros totales  
 Vendedor 103: 6 libros totales
 
-
-
 */
+
+#include <iostream>
+using namespace std;
+
+struct Venta {
+    int codigoVendedor;
+    int cantidadLibros;
+};
+
+void cargar(Venta ventas[], int cant) {
+    for (int i = 0; i < cant; i++) {
+        cout << "Venta " << i + 1 << ":" << endl;
+        cout << "Código de vendedor: ";
+        cin >> ventas[i].codigoVendedor;
+        cout << "Cantidad de libros vendidos: ";
+        cin >> ventas[i].cantidadLibros;
+    }
+}
+
+void listarVentasPorVendedor(Venta ventas[], int n) {
+    int i = 0;
+
+    // primer ciclo es el que recorre el lote completo
+    while (i < n) {
+        // guardo el valor de la clave o agrupador
+        // esto permite agrupar los registros por legajo
+        int key = ventas[i].codigoVendedor;
+        
+        // inicializado contadores , acumuladores etc de cada sublote
+        int totalLibros = 0;
+
+        // segundo ciclo se mantine por el sublote mientras sea el mismo
+        // codigo de vendedor y no se haya acabado el vector
+        while (i < n && key == ventas[i].codigoVendedor) {
+            totalLibros += ventas[i].cantidadLibros;
+            i++;
+        }
+
+        cout << "Vendedor " << key << ": " << totalLibros << " libros totales" << endl;
+    }
+}
+
+
+int main () {
+
+    Venta ventas[] = {
+        {101, 5},
+        {101, 3},
+        {102, 7},
+        {102, 2},
+        {102, 4},
+        {103, 6}
+    };
+    
+    int cantidad = sizeof(ventas) / sizeof(ventas[0]);
+    listarVentasPorVendedor(ventas, cantidad);
+    
+    return 0;
+}
